1c2f56c87c8b42876288b5396d7a1bd293a90593
Lucasandre23 <lucasandre152015@gmail.com>
2025-10-15 21:03:19 -0300

    feat: Adiciona models iniciais (Livro, Autor, Editora) e registro no Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import Autor, Editora, Livro 
 # Register your models here.
+
+
+
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    
+    list_display = ('nome',) 
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+
+    list_display = ('nome',)
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    
+    list_display = ('titulo', 'ISBN', 'publicacao', 'estoque', 'editora')
+    
+    
+    list_filter = ('editora', 'publicacao') 
+    
+    
+    filter_horizontal = ('autores',)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+    # Create your models here.
+class Editora(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+
+        return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nome
+        
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    ISBN = models.CharField(max_length=13, unique=True)
+    publicacao = models.DateField()
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    estoque = models.IntegerField()
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    autores = models.ManyToManyField(Autor)
+
+    def __str__(self):
+        return self.titulo  
+
+
+        
